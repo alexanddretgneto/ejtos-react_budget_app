177cfc4c81fe2fd24270b1a0c0f74d5a08d86692
alexanddretgneto <alexanddretgneto@gmail.com>
2023-05-27 08:37:00 -0400

    novo
    Signed-off-by: alexanddretgneto <alexanddretgneto@gmail.com>

---
+++ src/components/Courrence.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+
+const Courrence = () => {
+    
+    return (
+                <label className='alert alert-success'>
+                    Courrence
+                <select className='alert alert-success'>
+                    <option >$ Dollar</option>
+                    <option >£ Pound</option>
+                    <option >€ Euro</option>
+                    <option >₹ Rupee</option>
+                </select>
+                </label>          
+    );
+};
+export default Courrence;
