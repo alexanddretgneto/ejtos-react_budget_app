1b019688db0f2d8cc0dee6ca20cede4d4215a7ec
alexanddretgneto <alexanddretgneto@gmail.com>
2023-05-30 18:07:23 -0400

    atualização 2
    Signed-off-by: alexanddretgneto <alexanddretgneto@gmail.com>

--- src/components/Location.js
+++ src/components/Location.js
@@ -15,7 +15,7 @@ const Location = () => {
   return (
         <div className='alert alert-secondary'> Location {
          <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
-        <option value="$">Dollar</option>
+        <option value="$">Dollar($)</option>
         <option value="£">Pound(£)</option>
         <option value="€">Euro(€)</option>
         <option value="₹">Ruppee(₹)</option>
